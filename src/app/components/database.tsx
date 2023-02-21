ed6feba81ffb6af98625a7cac54a39d28fe9b004
AhmedRazaShaikh <110739149+xtylershona@users.noreply.github.com>
2023-02-21 23:02:50 +0500

    syllabus page

--- src/app/components/database.tsx
+++ src/app/components/database.tsx
@@ -36,3 +36,87 @@ export const data = [
         text:'Panaverse DAO is a community of Web 3 and Metaverse developers, designers, trainers, startup founders'
     }
 ]
+
+export const cards = [
+    {
+        id:1,
+        heading1:'1- Web 3.0 (Blockchain) and Metaverse Specialization',
+        text1:'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.' ,
+        heading2:'Quarter IV',
+        text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        heading3:'Quarter V',
+        text3:'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences' ,
+    },
+    {
+        id:2,
+        heading1:'2- Web 3.0 (Blockchain) and Metaverse Specialization',
+        text1:'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.' ,
+        heading2:'Quarter IV',
+        text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        heading3:'Quarter V',
+        text3:'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences' ,
+    },
+    {
+        id:3,
+        heading1:'3- Web 3.0 (Blockchain) and Metaverse Specialization',
+        text1:'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.' ,
+        heading2:'Quarter IV',
+        text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        heading3:'Quarter V',
+        text3:'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences' ,
+    },
+    {
+        id:4,
+        heading1:'4- Web 3.0 (Blockchain) and Metaverse Specialization',
+        text1:'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.' ,
+        heading2:'Quarter IV',
+        text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        heading3:'Quarter V',
+        text3:'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences' ,
+    },
+    {
+        id:5,
+        heading1:'5- Web 3.0 (Blockchain) and Metaverse Specialization',
+        text1:'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.' ,
+        heading2:'Quarter IV',
+        text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        heading3:'Quarter V',
+        text3:'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences' ,
+    },
+    {
+        id:6,
+        heading1:'6- Web 3.0 (Blockchain) and Metaverse Specialization',
+        text1:'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.' ,
+        heading2:'Quarter IV',
+        text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        heading3:'Quarter V',
+        text3:'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences' ,
+    },
+    {
+        id:7,
+        heading1:'7- Web 3.0 (Blockchain) and Metaverse Specialization',
+        text1:'This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.' ,
+        heading2:'Quarter IV',
+        text2: 'W3-351: Developing Smart Contracts and Planet-Scale Web 3.0 Dapps',
+        heading3:'Quarter V',
+        text3:'MV-361: Developing Planet-Scale and Augmented Metaverse Experiences' ,
+    },
+]
+
+export const core = [
+    {
+        id:1,
+        heading:'Quarter I',
+        text:'CS-101: Object-Oriented Programming using TypeScript and Typescript for React'
+    },
+    {
+        id:2,
+        heading:'Quarter II',
+        text:'W2-201: Developing Planet-Scale Web 2.0 Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform'
+    },
+    {
+        id:3,
+        heading:'Quarter III',
+        text:'$-101: Dollar Making Bootcamp - Full-Stack Template and API Product Development'
+    },
+]
--- src/app/syllabus/page.tsx
+++ src/app/syllabus/page.tsx
@@ -1,9 +1,64 @@
 'use client'
+import { Box, Center, Container, Flex, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 import Home from '../components/Home'
+import { cards, core } from '../components/database'
 
 export default function page() {
   return (
-    <Home title='Syllabus' src='https://background.live/wp-content/uploads/2022/04/41613405958rhvilahlttorlzyz8umwdaek6npyr63gswnshyoalqcmdjkwzjxvtjvzmllqd3kbgrcmpza1j4w7bzbsvk3iishphloe71gpkg6t.jpg' />
+    <>
+      <Home title='Syllabus' src='https://background.live/wp-content/uploads/2022/04/41613405958rhvilahlttorlzyz8umwdaek6npyr63gswnshyoalqcmdjkwzjxvtjvzmllqd3kbgrcmpza1j4w7bzbsvk3iishphloe71gpkg6t.jpg' />
+      <Box pt='30px'>
+        <Container maxW={1400}>
+          <Center flexDir='column'>
+            <Heading size='2xl'>Course Syllabus</Heading>
+            <Text mt='20px' textAlign='center' px='130px'>The first three quarters are shared by all specialties and are dedicated to studying Object-Oriented Programming and cutting-edge Full-Stack Web 2.0 development. It is going to be a fifteen-month-long hybrid program that includes both onsite and online classes and is divided into five quarters of 13 weeks each</Text>
+          </Center>
+          <Center flexDir='column' mt='20px'>
+            <Heading>Common In All</Heading>
+            <Text textAlign='center' px='130px'>Every participant of the program will start by completing the following three core courses</Text>
+          </Center>
+          
+          <Center>
+            <Flex mt='30px' gap='20px'>
+              {core.map((elem) => ( 
+              <Box textAlign='center' p='30px' boxShadow='dark-lg' borderRadius='20px' width='290px' key={elem.id}>
+                <Heading>{elem.heading}</Heading>
+                <Text pt='5px'>{elem.text}</Text>
+              </Box>
+              ))}
+            </Flex>
+          </Center>
+        </Container>
+
+
+
+        <Container maxW={1400} mt='30px' bgSize='cover' bgAttachment='fixed' bgImage='https://png.pngtree.com/background/20210709/original/pngtree-spray-powder-glare-colorful-background-picture-image_623477.jpg'>
+          {cards.map((elem) => (
+            <Box pt='40px' key={elem.id}>
+              <Center flexDir='column' mt='50px'>
+                <Heading size='xl'>{elem.heading1}</Heading>
+                <Text fontWeight='medium' color='white' mt='20px' textAlign='center' px='130px'>{elem.text1}</Text>
+              </Center>
+              <Center>
+                <Flex mt='30px' gap='20px'>
+                  <Box bgColor='white' textAlign='center' p='50px' boxShadow='dark-lg' borderRadius='20px' width='350px'>
+                    <Heading>{elem.heading2}</Heading>
+                    <Text fontWeight='medium' pt='5px'>{elem.text2}</Text>
+                  </Box>
+                  <Box bgColor='white' textAlign='center' p='50px' boxShadow='dark-lg' borderRadius='20px' width='350px'>
+                    <Heading>{elem.heading3}</Heading>
+                    <Text fontWeight='medium' pt='5px'>{elem.text3}</Text>
+                  </Box>
+                </Flex>
+              </Center>
+            </Box>
+          ))}
+
+
+        </Container>
+      </Box>
+
+    </>
   )
 }
